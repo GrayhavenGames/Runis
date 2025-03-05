013815f6f863a4d63ed8d2f43e84c55a392d0ffa
PaxtonTerryDev <paxtonterrydev@gmail.com>
2025-03-04 17:20:36 -0700

    initial build out of attribute system. Defined signals

--- Attributes/Attribute.cs
+++ Attributes/Attribute.cs
@@ -1,6 +1,43 @@
 using Godot;
-using System;
 
+namespace Runis.Attributes;
+[GlobalClass]
 public partial class Attribute : Node
 {
+    [Signal]
+    public delegate void PreBaseValueChangedEventHandler(float oldValue, float newValue);
+    [Signal]
+    public delegate void BaseValueChangedEventHandler(float oldValue, float newValue);
+    [Signal]
+    public delegate void PreCurrentValueChangedEventHandler(float oldValue, float newValue);
+    [Signal]
+    public delegate void CurrentValueChangedEventHandler(float oldValue, float newValue);
+    
+    private float _baseValue;
+    private float _currentValue;
+
+    public float BaseValue
+    {
+        get => _baseValue;
+        set
+        {
+            EmitSignal(SignalName.PreBaseValueChanged, _currentValue, value);
+            var oldValue = _currentValue;
+            _currentValue = value;
+            EmitSignal(SignalName.BaseValueChanged, oldValue, _currentValue);
+        }
+    }
+
+    public float CurrentValue
+    {
+        get => _currentValue;
+        set
+        {
+            EmitSignal(SignalName.PreCurrentValueChanged, _currentValue, value);
+            var oldValue = _currentValue;
+            _currentValue = value;
+            EmitSignal(SignalName.CurrentValueChanged, oldValue, _currentValue);
+        }
+    }
+    
 }
---
+++ Attributes/AttributeSet.cs
@@ -0,0 +1,8 @@
+using Godot;
+using Godot.Collections;
+
+namespace Runis.Attributes;
+[GlobalClass]
+public partial class AttributeSet: Node
+{
+}
